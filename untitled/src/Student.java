5c763a781f975d99caf9ee4bdc3dc07ade2f7681
s30325 <162446731+s30325@users.noreply.github.com>
2024-04-24 14:47:54 +0200

    Update Student.java

--- untitled/src/Student.java
+++ untitled/src/Student.java
@@ -1,5 +1,8 @@
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+import java.util.HashMap;
+
 public class Student {
     private String fname;
     private String lname;
@@ -7,8 +10,17 @@ public class Student {
     private String adres;
     private String phoneNumber;
     private Date dateOfBirth;
+    private String studentStatus;
     private String indexNumber;
-    static ArrayList<Student>students = new ArrayList<Student>();
+    private int currentSemester;
+    private int studentITNnumber;
+    private StudyProgramme currentStudyPrograme;
+    public HashMap<Integer, String>grades = new HashMap<>();
+
+    public static ArrayList<Student>students = new ArrayList<Student>();
+
+
+
     public Student (String fname, String lname, String email, String adres, String phoneNumber, Date dateOfBirth){
         this.fname = fname;
         this.lname = lname;
@@ -16,12 +28,29 @@ public class Student {
         this.adres = adres;
         this.phoneNumber = phoneNumber;
         this.dateOfBirth = dateOfBirth;
+        this.currentSemester = 1;
         Student.students.add(this);
         this.indexNumber = "s" + students.size();
     }
 
-    public void enrollStudent(StudyProgramme stProgramme){
 
+    public void enrollStudent(StudyProgramme stProgramme){
+this.currentStudyPrograme = stProgramme;
+this.studentStatus = "Student";
+    }
+    public void addGrade(int grade,String subject){
+        grades.put(grade,subject);
+    }
+public void promoteNextSemester(){
+        if(currentSemester<currentStudyPrograme.getSemesterNumber() && studentITNnumber < currentStudyPrograme.getITNnumber() ){
+            if(currentSemester<currentStudyPrograme.getSemesterNumber()){
+            currentSemester++;}
+         else{
+            studentStatus = "Absolwent";
+        }}
+        else{
+            setCurrentSemester(1);
+        }
     }
 
     public String getFname() {
@@ -71,5 +100,40 @@ public class Student {
     public void setDateOfBirth(Date dateOfBirth) {
         this.dateOfBirth = dateOfBirth;
     }
+    public int getCurrentSemester() {
+        return currentSemester;
+    }
+    public void setCurrentSemester(int currentSemester) {
+        this.currentSemester = currentSemester;}
 
+ public String getStudentStatus() {
+    return studentStatus;
 }
+
+    public void setStudentStatus(String studentStatus) {
+        this.studentStatus = studentStatus;
+    }
+
+    public String getIndexNumber() {
+        return indexNumber;
+    }
+
+    public void setIndexNumber(String indexNumber) {
+        this.indexNumber = indexNumber;
+    }
+
+    public int getStudentITNnumber() {
+        return studentITNnumber;
+    }
+
+    public void setStudentITNnumber(int studentITNnumber) {
+        this.studentITNnumber = studentITNnumber;
+    }
+
+    public StudyProgramme getCurrentStudyPrograme() {
+        return currentStudyPrograme;
+    }
+
+    public void setCurrentStudyPrograme(StudyProgramme currentStudyPrograme) {
+        this.currentStudyPrograme = currentStudyPrograme;
+    }}
